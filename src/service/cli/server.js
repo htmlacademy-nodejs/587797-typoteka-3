b28395696ab7f9b496d1b1b04c82effb6e9bf715
Sergey Shershnev <shv.sergey70@gmail.com>
2020-04-23 19:03:07 +0300

    feature: added logging

--- src/service/cli/server.js
+++ src/service/cli/server.js
@@ -2,6 +2,9 @@
 
 const chalk = require(`chalk`);
 const express = require(`express`);
+const {getLogger} = require(`../../libs/logger`);
+const logger = getLogger();
+
 const {
   HttpCode,
 } = require(`../../constants`);
@@ -14,14 +17,30 @@ const DEFAULT_PORT = 3000;
 
 const app = express();
 app.use(express.json());
+app.use((req, res, next) => {
+  logger.debug(`New request. Url: ${req.url}`);
+  const onResponseFinish = () => {
+    logger.info(`Request finished with code: ${res.statusCode}`);
+    logger.info(`All response info: ${res.toString()}`); // как распечатать объект, чтобы не было All response info: [object Object]
+    res.removeListener(`finish`, onResponseFinish);
+  };
+  res.on(`finish`, onResponseFinish);
+  next();
+});
 
 app.use(`/api/articles`, articlesRouter);
 app.use(`/api/categories`, categoriesRouter);
 app.use(`/api/search`, searchRouter);
 
-app.use((req, res) => res
-  .status(HttpCode.NOT_FOUND)
-  .send(`Not found`));
+app.use((req, res) => {
+  logger.error(`Trying to get non-existing route: ${req.url}`);
+  res.status(HttpCode.NOT_FOUND).send(`Not found`);
+});
+
+app.use((error, req, res, next) => { // Если поместить throw new Error в один из обработчиков маршрута - сюда мы так и не попадаем
+  logger.error(`Caught unexpected error for url: ${req.url}. Error: ${error}`);
+  res.status(HttpCode.INTERNAL_ERROR).send(`Server internal error!@!@`);
+});
 
 module.exports = {
   name: `--server`,
@@ -31,10 +50,10 @@ module.exports = {
 
     app.listen(port, (error) => {
       if (error) {
-        console.info(chalk.green(`Ошибка при запуске сервера`, error));
+        logger.error(chalk.green(`Can't launch server`, error));
       }
 
-      console.info(chalk.green(`Ожидаю соединений на порт ${port}`));
+      logger.info(chalk.green(`Server launched. Listening port: ${port}`));
     });
   }
 };
