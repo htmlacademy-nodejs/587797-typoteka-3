0e111897948c47aefbbde36b46ef127a2966c24a
Sergey Shershnev <shv.sergey70@gmail.com>
2020-05-12 20:36:57 +0300

    feature: complemented logic

--- src/express/routes/articles.js
+++ src/express/routes/articles.js
@@ -1,15 +1,28 @@
 'use strict';
 
+const fs = require(`fs`);
 const axios = require(`axios`);
 const path = require(`path`);
+const {getLogger} = require(`../libs/logger`);
+const logger = getLogger();
 const multer = require(`multer`);
-const upload = multer({
-  dest: path.resolve(__dirname, `../public/img`)
-});
+const mime = require(`mime/lite`);
+
+const InvalidFormException = require(`../exceptions/invalid-form`);
 
 const {
-  BASE_API_URL
+  BASE_API_URL,
+  HttpCode
 } = require(`../../constants`);
+
+const {
+  ARTICLES_PICTURES_DIR,
+  TMP_DIR
+} = require(`../constants`);
+
+const upload = multer({
+  dest: TMP_DIR
+});
 
 const {Router} = require(`express`);
 
@@ -25,14 +38,55 @@ articlesRouter
       article: {}
     });
   })
-  .post(`/add`, upload.single(`picture`), (req, res, next) => {
+  .post(`/add`, upload.single(`picture`), async (req, res, next) => {
+    let newFilePath = null;
     const requiredFields = [`title`, `announce`, `fullText`];
 
     try {
-      const picture = req.file;
-      const fields = req.body;
-    } catch (error) {
-      next(error);
+      logger.debug({message: `Got body`, content: req.body});
+      logger.debug({message: `Got file`, content: req.file});
+
+      const keysFromForm = Object.keys(req.body);
+
+      const areAllRequiredFieldsExist = requiredFields.every((requiredField) => keysFromForm.includes(requiredField));
+
+      if (!areAllRequiredFieldsExist) {
+        throw new InvalidFormException(`Not all required fields are presented. Fields from form: ${keysFromForm.toString()}.
+        Required fields: ${requiredFields.toString()}`);
+      }
+
+      const {
+        mimetype: mimeType,
+        size,
+        path: filePath,
+        originalname: originalName,
+        filename: generatedName
+      } = req.file;
+
+      if (size === 0 || ![`image/jpeg`, `image/png`, `image/webp`].includes(mimeType)) {
+        throw new InvalidFormException(`Empty file or bad mime type. Size ${size}, mimetype: ${mimeType}`);
+      }
+
+      const newFileName = `${generatedName}.${mime.getExtension(mimeType)}`;
+      newFilePath = path.resolve(ARTICLES_PICTURES_DIR, newFileName);
+
+      await fs.promises.rename(filePath, newFilePath);
+      logger.info(`File successfully renamed. Original filename: ${originalName}, generated filename: ${newFileName}`);
+
+      logger.info(`Form is valid. Send post request to record data`);
+
+      const postResponse = await axios
+        .post(`${BASE_API_URL}/api/offers`, Object.assign({}, req.body, {avatar: newFileName}));
+
+      logger.info(`Response status is ${postResponse.status}`);
+
+      if (postResponse.status === HttpCode.SUCCESS_POST) {
+        res.redirect(`/my`);
+      } else {
+        next(new Error(`Invalid response status code: ${postResponse.status}`));
+      }
+    } catch (caughtError) {
+      next(caughtError);
     }
   });
 articlesRouter.get(`/category/:id`, (req, res) => {
